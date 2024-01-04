f5b2225ee428b89531252817bb7b13360948ecfc
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-01-04 11:17:37 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -110,3 +110,18 @@ SELECT first_name,last_name FROM names WHERE first_name LIKE '______%'
 prepare draw_pattern(int) as
 SELECT REPEAT('*', n) AS star_pattern
 FROM generate_series($1, 1, -1) AS n;
+/*
+For this challenge you need to create a simple SELECT statement that will return all columns from the products table, and join to the companies table so that you can return the company name.
+
+products table schema
+id
+name
+isbn
+company_id
+price
+companies table schema
+id
+name
+You should return all product fields as well as the company name as "company_name". */
+SELECT products.*, companies.name AS company_name from products
+JOIN companies ON products.company_id = companies.id;
