f6ed6364313267fcfa2f6cbdcaeaa4e7284dd7f1
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-01-06 11:06:41 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -193,4 +193,17 @@ GROUP BY
   date
 ORDER BY
   date;
+/*
+For this challenge you need to create a simple GROUP BY statement, you want to group all the people by their age and count the people who have the same age.
+
+people table schema
+id
+name
+age
+select table schema
+age [group by]
+people_count (people count)
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
+SELECT COUNT(id) as people_count, age FROM people GROUP BY age;
 
