07cb49c57b8c59f575f8116029f09c5196e432b2
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-01-13 16:57:13 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -206,4 +206,21 @@ people_count (people count)
 NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
 */
 SELECT COUNT(id) as people_count, age FROM people GROUP BY age;
+/*
+Given a table of random numbers as follows:
 
+numbers table schema
+id
+number1
+number2
+Your job is to return table with similar structure and headings, where if the sum of a column is odd, the column shows the minimum value for that column, and when the sum is even, it shows the max value. You must use a case statement.
+
+output table schema
+number1
+number2
+*/
+SELECT
+  CASE WHEN SUM(number1) % 2 = 1 THEN MIN(number1) ELSE MAX(number1) END AS number1,
+  CASE WHEN SUM(number2) % 2 = 1 THEN MIN(number2) ELSE MAX(number2) END AS number2
+FROM
+  numbers;
