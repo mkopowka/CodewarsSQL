9474dc97020784eec00b5432252079c604d84a2f
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-02-05 20:15:01 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -302,3 +302,22 @@ SELECT race, COUNT(*) AS count
 FROM demographics
 GROUP BY race
 ORDER BY count DESC;
+/*
+You have reached a conclusion that you waste to much time because you have to many different warehouse to visit each week.
+
+You have to find out how many different type of products you buy from each producer. If you take only few items from some of them you will stop going there to save the gasoline:)
+
+In the results show producer and count_products_types which you buy from him.
+
+Order the result by count_products_types (DESC) then by producer (ASC) in case there are duplicate amounts,
+
+products table schema
+id
+name
+price
+stock
+producer
+results table schema
+count_products_types
+producer
+*/
