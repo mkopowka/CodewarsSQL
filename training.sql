0c9508004fc099751dda95cabfa5a42d2358281a
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-02-06 16:40:35 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -369,3 +369,14 @@ SELECT
     REPLACE(s, '!', '') AS res
 FROM 
     removeexclamationmarks;
+/* 
+For this challenge you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50
+
+people table schema
+id
+name
+age
+You should return all people fields where their age is over 50 and order by the age descending
+*/
+-- Create your SELECT statement here
+SELECT * FROM people where age > 50 ORDER BY age DESC;
