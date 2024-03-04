8133731d87a9e415e5e8690b1a9db33306f7ee8e
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-03-04 20:28:57 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -380,3 +380,52 @@ You should return all people fields where their age is over 50 and order by the 
 */
 -- Create your SELECT statement here
 SELECT * FROM people where age > 50 ORDER BY age DESC;
+/*
+You have access to two tables named top_half and bottom_half, as follows:
+
+top_half schema
+
+id
+heads
+arms
+bottom_half schema
+
+id
+legs
+tails
+You must return a table with the format as follows:
+
+output schema
+
+id
+heads
+legs
+arms
+tails
+species
+The IDs on the tables match to make a full monster. For heads, arms, legs and tails you need to draw in the data from each table.
+
+For the species, if the monster has more heads than arms, more tails than legs, or both, it is a 'BEAST' else it is a 'WEIRDO'. This needs to be captured in the species column.
+
+All rows should be returned (10).
+
+Tests require the use of CASE. Order by species.
+*/
+
+SELECT 
+    TH.ID,
+    TH.HEADS,
+    BH.LEGS,
+    TH.ARMS,
+    BH.TAILS,
+    CASE
+        WHEN TH.HEADS > TH.ARMS OR BH.TAILS > BH.LEGS THEN 'BEAST'
+        ELSE 'WEIRDO'
+    END AS SPECIES
+FROM 
+    TOP_HALF TH
+JOIN 
+    BOTTOM_HALF BH ON TH.ID = BH.ID
+ORDER BY 
+    SPECIES;
+
