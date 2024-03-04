2036b5c8fc1b002adb2631254cc2cb6930eef4f0
mateusz kopowka <34452426+mkopowka@users.noreply.github.com>
2024-03-04 20:38:21 +0100

    Update training.sql

--- training.sql
+++ training.sql
@@ -428,4 +428,42 @@ JOIN
     BOTTOM_HALF BH ON TH.ID = BH.ID
 ORDER BY 
     SPECIES;
+/*
+You have arrived at the Celadon Gym to battle Erika for the Rainbow Badge.
 
+She will be using Grass-type Pokemon. Any fire pokemon you have will be strong against grass, but your water types will be weakened. The multipliers table within your Pokedex will take care of that.
+
+Using the following tables, return the pokemon_name, modifiedStrength and element of the Pokemon whose strength, after taking these changes into account, is greater than or equal to 40, ordered from strongest to weakest.
+
+pokemon schema
+
+id
+pokemon_name
+element_id
+str
+multipliers schema
+
+id
+element
+multiplier
+*/
+
+
+SELECT
+    p.pokemon_name,
+    p.str * m.multiplier AS modifiedStrength,
+    CASE
+        WHEN m.element = 'Fire' THEN 'Fire'
+        WHEN m.element = 'Water' THEN 'Water'
+        WHEN m.element = 'Dragon' THEN 'Dragon'
+        WHEN m.element = 'Grass' THEN 'Grass'
+        ELSE 'Normal'
+    END AS element
+FROM
+    pokemon p
+JOIN
+    multipliers m ON p.element_id = m.id
+WHERE
+    p.str * m.multiplier >= 40
+ORDER BY
+    modifiedStrength DESC;
